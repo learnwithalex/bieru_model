2addb02fa9b9816db09fcde50956eb30fb820caf
Questgig <90308279+learnwithalex@users.noreply.github.com>
2023-06-17 23:16:26 +0200

    Create api_request.php

---
+++ api_request.php
@@ -0,0 +1,24 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://alexpaul-alexpaul-bieru-base.hf.space/run/predict',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS =>'{ "data": ["am feeling good today"] }
+',
+  CURLOPT_HTTPHEADER => array(
+    'Content-Type: application/json'
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
